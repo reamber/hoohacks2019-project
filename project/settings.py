e011926c6327b21265f6ac5d5a1f89c8f0633997
Amber Liu <AmberLiu@d-172-25-99-121.dhcp.virginia.edu>
2019-03-02 14:27:27 -0500

    changes to status for heroku

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zox%lihi52$rh(7a1rnpa%uv+p#j5#6f17$4fc53sw*%)aj8x2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
