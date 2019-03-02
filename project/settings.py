11de31dc5feac13280eff4245210af342a7c9c71
Jenny <jrw3mx@virginia.edu>
2019-03-02 13:58:26 -0500

    landing page

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reminder.apps.ReminderConfig',
+    'pages',
+    'bootstrap4',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('reminder/', include('reminder.urls')),
 ]
